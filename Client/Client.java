c8d5ddc207156d279ba0e93ac42e42a9ef79081a
DESKTOP-MKKAHMG\Ale <alemasverano@gmail.com>
2025-01-03 11:22:16 +0100

    Prima connessione tra client e server

--- Client/Client.java
+++ Client/Client.java
@@ -1,7 +1,33 @@
-class Client
-{
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class Client {
     public static void main(String[] args) 
     {
-        
+       Client.connessione();
+    }
+
+    public static void connessione()
+    {
+        try (DatagramSocket clientSocket = new DatagramSocket()) {
+            InetAddress serverAddress = InetAddress.getByName("localhost");
+            int serverPort = 12345;
+
+            String message = "Richiesta connessione";
+            byte[] buffer = message.getBytes();
+
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, serverAddress, serverPort);
+            clientSocket.send(packet);
+
+            byte[] responseBuffer = new byte[1024];
+            DatagramPacket responsePacket = new DatagramPacket(responseBuffer, responseBuffer.length);
+            clientSocket.receive(responsePacket);
+
+            String response = new String(responsePacket.getData(), 0, responsePacket.getLength());
+            System.out.println(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- Server/Server.java
+++ Server/Server.java
@@ -1,7 +1,40 @@
-class Server
-{
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class Server {
     public static void main(String[] args) 
     {
-        
+        Server.connessione();
+    }
+
+    public static void connessione()
+    {
+        try (DatagramSocket serverSocket = new DatagramSocket(12345)) {
+            System.out.println("Server in ascolto");
+            
+            byte[] buffer = new byte[1024];
+
+            int numHost = 0;
+
+            while (numHost < 2) {
+                DatagramPacket receivedPacket = new DatagramPacket(buffer, buffer.length);
+                serverSocket.receive(receivedPacket);
+
+                String receivedMessage = new String(receivedPacket.getData(), 0, receivedPacket.getLength());
+                if(receivedMessage.equals("Richiesta connessione"))
+                {
+                    String responseMessage = "Connessione stabilita";
+                    byte[] responseData = responseMessage.getBytes();
+                    DatagramPacket responsePacket = new DatagramPacket(responseData, responseData.length, receivedPacket.getAddress(), receivedPacket.getPort());
+                    serverSocket.send(responsePacket);
+                    numHost++;
+                    System.out.println("Client connesso");
+                }
+                
+            }
+            System.out.println("Inizio gioco");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
